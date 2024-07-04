56c12a0e1448acf2a4d06c1db4336c162b2edb74
lazuardh <habiyanlazuard21@gmail.com>
2024-07-04 14:25:24 +0700

    modified(card Article): grouping content

--- lib/features/article/presentation/screen/article_screen.dart
+++ lib/features/article/presentation/screen/article_screen.dart
@@ -21,61 +21,93 @@ class ArticleScreen extends StatelessWidget {
           Expanded(
             child: ListView.separated(
               itemCount: 10,
+              padding: EdgeInsets.symmetric(
+                vertical: 10,
+                horizontal: size.width * 0.05,
+              ),
               itemBuilder: (context, index) {
-                return GestureDetector(
+                return _BuildCardArticle(
+                  title:
+                      '7 Merk Semen Pilihan Terbaik di Indonesia untuk bangun rumah Impian',
+                  images: 'https://picsum.photos/200',
+                  category: 'Dinding',
                   onTap: () {},
-                  // onTap: () => Navigator.pushNamed(context, RouteName.detailArtikel),
-                  child: ColumnPadding(
-                    padding: EdgeInsets.symmetric(
-                        vertical: 10, horizontal: size.width * 0.05),
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          SizedBox(
-                            width: size.width * 0.5,
-                            child: Text(
-                              '7 Merk Semen Pilihan Terbaik di Indonesia untuk bangun rumah Impian',
-                              style: AppTextStyle.regular.copyWith(
-                                color: AppColors.neutral500,
-                              ),
-                              textAlign: TextAlign.justify,
-                            ),
-                          ),
-                          const CustomImageWrapper(
-                            image: 'https://picsum.photos/200',
-                            width: 130,
-                            height: 75,
-                            borderRadius: AppBorderRadius.normal,
-                            isNetworkImage: true,
-                          ),
-                        ],
-                      ),
-                      const Gap(height: 15),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            'Dinding',
-                            style: AppTextStyle.regular.copyWith(
-                              color: AppColors.primary900,
-                            ),
-                          ),
-                          const Icon(
-                            Icons.share,
-                            color: AppColors.neutral300,
-                            size: 20,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
                 );
               },
               separatorBuilder: (context, index) => const Divider(
                 color: AppColors.neutral200,
               ),
             ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BuildCardArticle extends StatelessWidget {
+  final Function() _onTap;
+  final String _title;
+  final String _images;
+  final String _category;
+  // ignore: use_super_parameters
+  const _BuildCardArticle({
+    Key? key,
+    required String title,
+    required String images,
+    required String category,
+    required Function() onTap,
+  })  : _title = title,
+        _images = images,
+        _category = category,
+        _onTap = onTap,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _onTap,
+      // onTap: () => Navigator.pushNamed(context, RouteName.detailArtikel),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              SizedBox(
+                width: ResponsiveUtils(context).getMediaQueryWidth() * 0.5,
+                child: Text(
+                  _title,
+                  style: AppTextStyle.regular.copyWith(
+                    color: AppColors.neutral500,
+                  ),
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              CustomImageWrapper(
+                image: _images,
+                width: 130,
+                height: 75,
+                borderRadius: AppBorderRadius.normal,
+                isNetworkImage: true,
+              ),
+            ],
+          ),
+          const Gap(height: 15),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                _category,
+                style: AppTextStyle.regular.copyWith(
+                  color: AppColors.primary900,
+                ),
+              ),
+              const Icon(
+                Icons.share,
+                color: AppColors.neutral300,
+                size: 20,
+              ),
+            ],
           ),
         ],
       ),
